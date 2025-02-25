97a6f829c6c2e11d6dd77b583f930da3be79ed21
Tiago Reis <ttrpk@hotmail.com>
2025-02-25 13:40:15 -0300

    Api Vendas

--- src/common/infrastructure/http/app.ts
+++ src/common/infrastructure/http/app.ts
@@ -2,12 +2,27 @@ import express from 'express'
 import cors from 'cors'
 import { routes } from './routes';
 import { errorHandler } from './middleware/errorHandler';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+        },
+    },
+    apis: [],
+}
+const swaggerSpec = swaggerJSDoc(options)
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 app.use(routes)
 app.use(errorHandler)
 
-export {app}
+export { app }
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -4,4 +4,5 @@ import { app } from "./app";
 
 app.listen(env.PORT, () => {
     console.log(`Server is Running on port ${env.PORT}`)
+    console.log('API Docs disponíveis na rota GET /docs')
 })
