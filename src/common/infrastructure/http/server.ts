4d2cabe81f53c4fb61959bdd3ef0e9e381f21f21
Tiago Reis <ttrpk@hotmail.com>
2025-02-26 13:56:58 -0300

    docker

--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,8 +1,15 @@
 import { env } from "../env";
+import { dataSource } from "../typeorm";
 import { app } from "./app";
 
+dataSource
+    .initialize()
+    .then(() => {
+        app.listen(env.PORT, () => {
+            console.log(`Server is Running on port ${env.PORT}`)
+            console.log('API Docs disponíveis na rota GET /docs')
+        })
+    }).catch((error) => {
+        console.error('Error starting server:', error)
+    });
 
-app.listen(env.PORT, () => {
-    console.log(`Server is Running on port ${env.PORT}`)
-    console.log('API Docs disponíveis na rota GET /docs')
-})
