87b39b1e6a79314aee563633f072de30db16375d
Tiago Reis <ttrpk@hotmail.com>
2025-03-18 13:46:12 -0300

    ConflictNameTest

--- src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
+++ src/products/infrastructure/in-memory/repositories/products-in-memory.repository.spec.ts
@@ -1,5 +1,7 @@
 import { NotFoundError } from "@/common/domain/errors/not-found-error"
 import { ProductsInMemoryRepository } from "./products-in-memory.repository"
+import { ProductsDataBuilder } from "../../testing/helpers/products-data-builder"
+import { ConflictError } from "@/common/domain/errors/conflict-error"
 
 describe('ProductsInMemoryRepository unit tests', () => {
     let sut: ProductsInMemoryRepository
@@ -14,6 +16,29 @@ describe('ProductsInMemoryRepository unit tests', () => {
                 new NotFoundError('Product with name fake_name not found'),  
             )
             await expect(() => sut.findByName('fake_name')).rejects.toBeInstanceOf(NotFoundError)  
+        })
+
+        it('should find a product by name', async () => {
+            const data = ProductsDataBuilder({name: 'Curso Node Js'})
+            sut.items.push(data)
+            const result = await sut.findByName('Curso Node Js')
+            expect(result).toStrictEqual(data)
+        })
+    })
+
+    describe('conflictingName', () => {
+        it('should throw error when product found', async () => {
+            const data = ProductsDataBuilder({ name: 'Curso Node Js' })
+            sut.items.push(data)
+            await expect(() => sut.conflictingName('Curso Node Js')).rejects.toThrow(
+                new ConflictError('Name already used on another product'),
+            )
+            await expect(() => sut.conflictingName('Curso Node Js')).rejects.toBeInstanceOf(ConflictError)
+        })
+
+        it('should not find a product by name', async () => {
+          expect.assertions(0)
+          await sut.conflictingName('Curso Node Js')
         })
     })
 })
--- src/products/infrastructure/in-memory/repositories/products-in-memory.repository.ts
+++ src/products/infrastructure/in-memory/repositories/products-in-memory.repository.ts
@@ -1,4 +1,5 @@
 import { ConflictError } from "@/common/domain/errors/conflict-error";
+import { NotFoundError } from "@/common/domain/errors/not-found-error";
 import { InMemoryRepository } from "@/common/domain/repositories/in-memory.repository";
 import { ProductModel } from "@/products/domain/models/products.model";
 import { ProductId, ProductsRepository } from "@/products/domain/repositories/products.repository";
@@ -10,7 +11,7 @@ export class ProductsInMemoryRepository extends InMemoryRepository<ProductModel>
     async findByName(name: string): Promise<ProductModel> {
         const product = this.items.find((item) => item.name === name);
         if (!product) {
-            throw new Error(`Product with name ${name} not found`);
+            throw new NotFoundError(`Product with name ${name} not found`);
         }
         return product
     }
@@ -28,7 +29,7 @@ export class ProductsInMemoryRepository extends InMemoryRepository<ProductModel>
     async conflictingName(name: string): Promise<void> {
                 const product = this.items.find((item) => item.name === name);
                 if(product) {
-                    throw new ConflictError(`Product with name ${name} already exists`);
+                    throw new ConflictError(`Name already used on another product`);
                 }
             }
         
